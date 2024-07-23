3fcc9f6b903f63a96f220d76128925bb8a5075d6
ValentinMandev <valentin.vkm@gmail.com>
2024-07-23 20:17:10 +0300

    Initial commit

---
+++ src/main/java/com/softuni/mehana/MehanaApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.mehana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MehanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MehanaApplication.class, args);
+	}
+
+}
