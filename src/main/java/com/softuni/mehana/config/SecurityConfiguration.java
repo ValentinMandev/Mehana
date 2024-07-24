6c3b10c113fdab09bb817ee98a104ea884c75933
ValentinMandev <valentin.vkm@gmail.com>
2024-07-25 01:10:29 +0300

    Seventh commit - 24 Jul 2024

--- src/main/java/com/softuni/mehana/config/SecurityConfiguration.java
+++ src/main/java/com/softuni/mehana/config/SecurityConfiguration.java
@@ -1,4 +1,37 @@
 package com.softuni.mehana.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/", "/home", "/register", "/about", "/contact", "/gallery", "/menu",
+                                        "/css/**", "/images/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .permitAll()
+                )
+                .logout(logout ->
+                        logout
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+
 }
