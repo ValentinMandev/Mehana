d4b68b4b69cb19f4fb1837203f83e18c4bedf6e7
ValentinMandev <valentin.vkm@gmail.com>
2024-07-25 22:25:27 +0300

    Edit Security Configuration file - 25 Jul 2024

--- src/main/java/com/softuni/mehana/config/SecurityConfiguration.java
+++ src/main/java/com/softuni/mehana/config/SecurityConfiguration.java
@@ -1,37 +1,52 @@
 package com.softuni.mehana.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/", "/home", "/register", "/about", "/contact", "/gallery", "/menu",
-                                        "/css/**", "/images/**").permitAll()
-                                .anyRequest().authenticated()
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(
+                        authorizeRequests ->
+                                authorizeRequests
+                                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                        .requestMatchers("/", "/about", "/contact", "/gallery", "menu",
+                                                "/users/login", "/users/register", "/error").permitAll()
+                                        .anyRequest()
+                                        .authenticated()
                 )
                 .formLogin(formLogin ->
                         formLogin
-                                .loginPage("/login")
-                                .permitAll()
+                                .loginPage("/users/login")
+                                .usernameParameter("username")
+                                .passwordParameter("password")
+                                .defaultSuccessUrl("/", true)
+                                // What will happen if the login fails
+                                .failureForwardUrl("/users/login")
                 )
-                .logout(logout ->
-                        logout
-                                .permitAll()
-                );
+                .logout(
+                        logout ->
+                                logout
+                                        .logoutUrl("/users/logout")
+                                        .logoutSuccessUrl("/")
+                                        .invalidateHttpSession(true)
+                )
+                .build();
+    }
 
-        return http.build();
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Pbkdf2PasswordEncoder
+                .defaultsForSpringSecurity_v5_8();
     }
 
 }
