8d0e172fb8bc5393eef1f07e638aae69e6212523
ValentinMandev <valentin.vkm@gmail.com>
2026-01-09 21:59:48 +0200

    Configuration for Railway deployment

--- src/main/java/com/softuni/mehana/controller/user/LoginController.java
+++ src/main/java/com/softuni/mehana/controller/user/LoginController.java
@@ -1,16 +1,13 @@
 package com.softuni.mehana.controller.user;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/user/login")
-    public String login(@RequestParam(value = "returnUrl", required = false) String returnUrl, Model model) {
-        model.addAttribute("returnUrl", returnUrl);
+    public String login() {
         return "login";
     }
 
