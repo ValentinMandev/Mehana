05f301399e7abbdf66e64148d0afe27028be1e97
ValentinMandev <valentin.vkm@gmail.com>
2026-01-22 20:26:00 +0200

    Migrate to PostgreSQL

--- init-databases.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS mehana;
-CREATE DATABASE IF NOT EXISTS mehana_orders;
-
-GRANT ALL PRIVILEGES ON mehana.* TO 'mehana_user'@'%';
-GRANT ALL PRIVILEGES ON mehana_orders.* TO 'mehana_user'@'%';
-
-FLUSH PRIVILEGES;
